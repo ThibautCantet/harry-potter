e2967befe7404ecbaa6efba534683ae517a3caaf
Thibaut Cantet <thibaut.cantet@soat.fr>
2021-11-15 13:48:48 +0100

    refacto

--- src/main/java/com/kata/harry_potter/Panier.java
+++ src/main/java/com/kata/harry_potter/Panier.java
@@ -15,18 +15,23 @@ public class Panier {
         if (livres.isEmpty()) {
             return 0;
         } else {
-            double discount = 1;
-            if (livres.keySet().size() == 3) {
-                discount = 0.9;
-            }
-            if (livres.keySet().size() == 2) {
-                discount = 0.95;
-            }
+            double discount = computeDiscount();
 
             Integer nombreTotalDeLivres = livres.values().stream()
                     .reduce(Integer::sum).orElse(0);
             return nombreTotalDeLivres * Livre.PRIX * discount;
         }
+    }
+
+    private double computeDiscount() {
+        double discount = 1;
+        if (livres.keySet().size() == 3) {
+            discount = 0.9;
+        }
+        else if (livres.keySet().size() == 2) {
+            discount = 0.95;
+        }
+        return discount;
     }
 
     public void ajouter(int quantite, Livre livre) {
